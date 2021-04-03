ef2ac83692db1ebacdd4ceb83cbcc02f6312ff6d
Laptop-DOM <pducduy99@gmail.com>
2021-04-03 21:20:03 +0700

    .

--- fontend/js/calender.js
+++ fontend/js/calender.js
@@ -1,31 +1,25 @@
 
 
-function addColumn() {
-    document.getElementById("test").innerHTML = "hello1";
+function addColumn(roomNumber) {
+    var tbl = document.getElementsByTagName("table")[0];
+    var tr = tbl.getElementsByTagName("tr");
 
-    tbl = document.getElementsByTagName("table")[0];
-    tr = tbl.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-
-        var td = document.createElement('td');
-        var input = document.createElement('text');
-        td.appendChild(input);
-        tr[i].appendChild(td);
+    for (let i = 0; i < tr.length; i++) {
+        var trow;
+        if (i == 0) {
+            trow = document.createElement('th');
+            var input = document.createElement("div");
+            input.innerHTML = "Room " + roomNumber;
+            trow.appendChild(input);
+        } else {
+            trow = document.createElement('td');
+        }
+        tr[i].appendChild(trow);
     }
 }
 
-tbl = document.getElementsByTagName("table")[0];
-tr = tbl.getElementsByTagName("tr");
-for (i = 0; i < tr.length; i++) {
-    var trow;
-    if (i == 0) {
-        trow = document.createElement('th');
-        var input = document.createElement("div");
-        input.innerHTML = "Room 1";
-        trow.appendChild(input);
-    } else {
-        trow = document.createElement('td');
+$(document).ready(function () {
+    for(let i=1; i<= 3; i++){
+        addColumn(i);
     }
-
-    tr[i].appendChild(trow);
-}
+});
